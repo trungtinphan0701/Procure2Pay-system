cf6c2e3cc4374cd67c176a019d6a0157929b01c1
Tin <tin.trung.0701@gmail.com>
2024-07-02 16:26:57 +1000

    Initialise Project

---
+++ src/main/java/hosi/procure2pay/Procure2PayApplication.java
@@ -0,0 +1,13 @@
+package hosi.procure2pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Procure2PayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Procure2PayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hosi/procure2pay/Procure2PayApplicationTests.java
@@ -0,0 +1,13 @@
+package hosi.procure2pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Procure2PayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
