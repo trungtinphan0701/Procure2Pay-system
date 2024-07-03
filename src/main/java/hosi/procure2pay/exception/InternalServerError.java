ac3a1cc267f79aa91d3ddcffc6cad3293e8dd62b
jamesngn <103204978+jamesngn@users.noreply.github.com>
2024-07-03 14:42:01 +1000

    update code

---
+++ src/main/java/hosi/procure2pay/exception/InternalServerError.java
@@ -0,0 +1,40 @@
+package hosi.procure2pay.exception;
+
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+import org.springframework.http.HttpStatus;
+
+public enum InternalServerError implements ResponseError {
+    UNKNOWN(0, "UNKNOWN"),
+    INTERNAL_SERVER_ERROR(100, "INTERNAL ERROR");
+
+    private int errorCode;
+    private String message;
+
+    private InternalServerError(int errorCode, String message) {
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+
+    public String getName() {
+        return this.name();
+    }
+
+    public int getStatus() {
+        return HttpStatus.INTERNAL_SERVER_ERROR.value();
+    }
+
+    public Integer getCode() {
+        return this.errorCode;
+    }
+
+    public int getErrorCode() {
+        return this.errorCode;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
