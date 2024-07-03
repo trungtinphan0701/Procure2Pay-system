9dbc82b1687610f9430b0f85745f950e71c8b40c
jamesngn <103204978+jamesngn@users.noreply.github.com>
2024-07-03 18:43:32 +1000

    update bad request error

--- src/main/java/hosi/procure2pay/exception/BadRequestError.java
+++ src/main/java/hosi/procure2pay/exception/BadRequestError.java
@@ -7,8 +7,17 @@ import java.util.Arrays;
 public enum BadRequestError implements ResponseError {
     UNKNOWN(0, "Unknown error"),
     BAD_REQUEST_ERROR(100, "Bad request error"),
-    USER_ID_INVALID(101, "User id is invalid"),
-    USER_NOT_FOUND(102, "User not found"),
+    USER_ID_NULL(99, "User id is null"),
+    USER_ID_NOT_FOUND(101, "User id not found"),
+    FIRST_NAME_INVALID(104, "First name is invalid"),
+    LAST_NAME_INVALID(109, "Last name is invalid"),
+    USER_EMAIL_NULL(106, "User email is null"),
+    USER_EMAIL_ALREADY_EXISTS(102, "User email already exists"),
+    USER_EMAIL_NOT_FOUND(103, "User email not found"),
+    USER_NOT_FOUND(105, "User not found"),
+    SUPPLIER_ID_NULL(107, "Supplier id is null"),
+    SUPPLIER_NOT_FOUND(108, "Supplier not found"),
+
     ;
 
 
