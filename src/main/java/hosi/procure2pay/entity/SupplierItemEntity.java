3ddbd4d24c8195e64f9c06778411b2c2214c1bc8
jamesngn <103204978+jamesngn@users.noreply.github.com>
2024-07-04 12:19:59 +1000

    API: init supplier item entity

--- src/main/java/hosi/procure2pay/entity/SupplierItemEntity.java
+++ src/main/java/hosi/procure2pay/entity/SupplierItemEntity.java
@@ -1,4 +1,33 @@
 package hosi.procure2pay.entity;
 
+import hosi.procure2pay.model.enums.ItemAvailability;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+@Table(name="supplier_items")
 public class SupplierItemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "supplier_id")
+    private SupplierEntity supplier;
+
+    private String name;
+
+    private String description;
+
+    @Column(name = "unit_cost", nullable = false)
+    private Float unitCost;
+
+    @Enumerated(EnumType.STRING)
+    private ItemAvailability availability;
+
+    @OneToOne(mappedBy = "supplierItem")
+    private RequisitionItemEntity requisitionItem;
 }
