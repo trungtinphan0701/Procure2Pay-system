9895654de811ccdf96e9188db0d4ba144b26f9a1
jamesngn <103204978+jamesngn@users.noreply.github.com>
2024-07-19 21:07:34 +1000

    chore: change requisition info search response to match with frontend (assigned to Tin Phan)

--- src/main/java/hosi/procure2pay/model/response/Requisition/GetRequisitionInfoResponse.java
+++ src/main/java/hosi/procure2pay/model/response/Requisition/GetRequisitionInfoResponse.java
@@ -5,14 +5,17 @@ import hosi.procure2pay.model.response.User.UserInfoResponse;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
 @RequiredArgsConstructor
 public class GetRequisitionInfoResponse {
+    private Integer requisitionId; // for document number
+    private String supplierName; // origin name
+    private LocalDate deliveryDate; // delivery date
     private UserInfoResponse createdBy;
     private String createdOn;
     private String state;
     private List<CreateRequisitionItemResponse> items;
-
 }
